dde1dc80e34a937af3a78b1b3b5f11585d101420
Gorka <gorka@aquigorka.com>
2017-06-09 09:14:13 +0200

    Modifies docker script to use latest changes for travis ci

--- src/scripts/docker.sh
+++ src/scripts/docker.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --volume "$PWD/src:/go/src/go-server-starter" --volume "$PWD/lib:/go/src" --name go-server-starter --workdir "/go/src/go-server-starter" --entrypoint /bin/bash -p 8877:8080 golang
+docker run -it --rm --volume "$PWD/:/go/src/github.com/AquiGorka/go-server-starter" --volume "$PWD/lib:/go/src" --name go-server-starter --workdir "/go/src/github.com/AquiGorka/go-server-starter" --entrypoint /bin/bash -p 8877:8080 golang
