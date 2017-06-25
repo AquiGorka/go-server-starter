db07a28254d8bba5eb2eb0eec978b629d4cc84fd
Gorka <gorka@aquigorka.com>
2017-06-25 22:08:49 +0200

    Modifies docker script to generate pkg folder rather than lib

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,1 +1,9 @@
-docker run -it --rm --volume "$PWD/main.go:/go/src/github.com/AquiGorka/go-server-starter/main.go" --volume "$PWD/server:/go/src/github.com/AquiGorka/go-server-starter/server" --volume "$PWD/scripts:/go/src/github.com/AquiGorka/go-server-starter/scripts" --volume "$PWD/lib:/go/src" --name go-server-starter --workdir "/go/src/github.com/AquiGorka/go-server-starter" --entrypoint /bin/bash -p 8877:8080 golang
+docker run -it --rm \
+  --volume "$PWD/main.go:/go/src/github.com/AquiGorka/go-server-starter/main.go" \
+  --volume "$PWD/server:/go/src/github.com/AquiGorka/go-server-starter/server" \
+  --volume "$PWD/scripts:/go/src/github.com/AquiGorka/go-server-starter/scripts" \
+  --volume "$PWD/pkg:/go/src" \
+  --name go-server-starter \
+  --workdir "/go/src/github.com/AquiGorka/go-server-starter" \
+  --entrypoint /bin/bash \
+  -p 8877:8080 golang
